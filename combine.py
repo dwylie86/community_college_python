8fde373d7260cfc4774918951904a452cdc13733
David Wylie <dwylie86@gmail.com>
2023-06-24 10:47:08 -0700

    Update combine.py

--- combine.py
+++ combine.py
@@ -1,5 +1,6 @@
 # David Wylie
 # PA 2
+# Takes three words, slices them to combines into a new word.
 
 slice_1 = input("Enter a word that contains at least 4 letters: ")[:3]
 slice_2 = input("Enter a word that contains at least 4 letters: ")[-2:]
