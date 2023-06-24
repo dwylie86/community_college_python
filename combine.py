03cf8f4828052471e6341cee77d136cdd4646ef9
David Wylie <dwylie86@gmail.com>
2023-06-24 10:46:39 -0700

    Create combine.py
    Takes three words, slices them to combines into a new word.

---
+++ combine.py
@@ -0,0 +1,8 @@
+# David Wylie
+# PA 2
+
+slice_1 = input("Enter a word that contains at least 4 letters: ")[:3]
+slice_2 = input("Enter a word that contains at least 4 letters: ")[-2:]
+slice_3 = input("Enter a word that contains at least 4 letters: ")[0]
+
+print(f"The combined word is: {slice_1}{slice_2}{slice_3}")
